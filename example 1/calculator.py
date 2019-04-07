77fa2bee432654becc28be940d935d0427738e6b
MercyRima <macrimah60@gmail.com>
2019-04-07 21:09:00 +0300

    added two files

---
+++ example 1/calculator.py
@@ -0,0 +1,38 @@
+def add (a,b):
+	answer=a+b
+	return answer
+
+def subtract(a,b):
+	answer=a-b
+	return answer
+
+def multiplication(a,b):
+	answer=a*b
+	return answer
+	
+def division(a,b):
+	answer=a/b
+	return answer
+
+def modulus(a,b):
+	answer=a%b
+	return answer
+
+def percentage(a,b):
+	answer=(a/b)*100
+	return answer
+
+def exponent(a,b):
+	answer=a**b
+	return answer
+
+def whole_division(a,b):
+	answer=a//b
+	return answer
+	
+
+	
+	
+	
+	
+	
